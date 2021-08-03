dd9195b84bafab853441cd1eab6d24669668b0a0
Kematzy <kematzy@gmail.com>
2021-08-03 19:45:54 +0800

    ESLint & Prettier fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2019
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['svelte3'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['warn', 'single'],
+    semi: ['warn', 'never'],
+    'comma-dangle': ['warn', 'always-multiline'],
+  },
+}
